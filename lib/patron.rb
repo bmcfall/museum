cb1714bf72d5d0365acbf7d54c5977394ef58dc0
Blythe McFall <mcfall.blythe@gmail.com>
2018-03-28 09:40:10 -0600

    Added some testing and a class.

---
+++ lib/patron.rb
@@ -0,0 +1,6 @@
+class Patron
+
+  
+
+
+end
---
+++ test/patron_test.rb
@@ -0,0 +1,15 @@
+require "minitest/autorun"
+require "minitest/pride"
+require "./lib/patron"
+
+class PatronTest < Minitest::Test
+
+  def test_patron_exists
+    patron = Patron.new
+  end
+
+  def test_patron_has_name
+    patron = Patron.new
+
+    
+end
